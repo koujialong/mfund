62de578cdfb707c07b4f13216fa0f18e874b3aab
koujialong <jialongkou@163.com>
2021-01-06 19:29:18 +0800

    webpack

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const path = require('path')
 module.exports = {
     devServer: {
         host: '0.0.0.0',
@@ -21,6 +22,20 @@ module.exports = {
         },
         headers: {
             'Access-Control-Allow-Origin': '*',
+        }
+    },
+    pages: {
+        index: {
+            // page 的入口
+            entry: 'src/main.ts',
+            // 模板来源
+            template: 'public/index.html',
+            // 在 dist/index.html 的输出
+            filename: 'index.html',
+            // 当使用 title 选项时，
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
         }
     }
 };
